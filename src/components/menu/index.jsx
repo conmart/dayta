c792adfc34e20c2c07bf8726c9dbc9232dd8c8b7
conmart <connor.a.martinelli@gmail.com>
2020-05-13 16:07:32 -0700

    native module styling

--- src/components/menu/index.jsx
+++ src/components/menu/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-import './styles.css'
+import styles from './menu.module.css';
 
 const Menu = () => {
   return (
-      <div className='menu'>
+      <div className={styles.container}>
         <NavLink to='/'>Today</NavLink>
         <NavLink to='/calendar'>Calendar</NavLink>
       </div>
