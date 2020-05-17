c8829c3db63c089a5c2be1488288909196392a59
conmart <connor.a.martinelli@gmail.com>
2020-05-17 15:31:03 -0700

    dummy data and first draft view of show category

--- src/components/singleCategory/showCategory.jsx
+++ src/components/singleCategory/showCategory.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
+import { Table } from 'antd';
 import { DeleteOutlined, UnorderedListOutlined } from '@ant-design/icons';
 
 import styles from './category.module.css';
+import 'antd/dist/antd.css';
+
+import { showSource, showColumns } from './dummyData';
 
 const ShowCategory = ({ showEventList }) => {
+  // TODO: add top display of most recent category occurance
+
   return (
-    <div>
-      show category
+    <div className={styles.showContainer}>
+      <Table 
+        dataSource={showSource}
+        columns={showColumns}
+        pagination={false}
+      />
       <div className={styles.showIconsContainer}>
         <div className={styles.showIcon}>
           <DeleteOutlined />
