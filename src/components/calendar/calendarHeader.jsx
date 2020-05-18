df2c8273224646a2367d1e23136fc8478c009187
conmart <connor.a.martinelli@gmail.com>
2020-05-18 15:37:50 -0700

    better header styling

--- src/components/calendar/calendarHeader.jsx
+++ src/components/calendar/calendarHeader.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { LeftOutlined, RightOutlined } from '@ant-design/icons';
 import { Select } from 'antd';
+import classNames from 'classnames';
 
-import { months, years } from './utils'
+import { months, years } from './utils';
 
 import styles from './calendar.module.css';
 
@@ -10,11 +11,14 @@ const { Option } = Select;
 
 const CalendarHeader = ({ selectedDate, setMonth, setYear }) => {
   const currentMonth = selectedDate.month();
+  const titleStyles = classNames('pageTitle', {
+    [styles.optionsContainer]: true,
+  });
 
   return (
     <div className={styles.titleContainer}>
       <LeftOutlined onClick={() => setMonth(currentMonth - 1)} />
-      <div className="pageTitle">
+      <div className={titleStyles}>
         <Select value={currentMonth} onChange={setMonth}>
           {months.map((month, index) => (
             <Option value={index} key={month}>
