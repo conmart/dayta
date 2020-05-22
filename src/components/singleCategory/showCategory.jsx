dd282820695425bd8eee6a932d45d97d84ed8329
conmart <connor.a.martinelli@gmail.com>
2020-05-21 18:08:53 -0700

    small style fixes

--- src/components/singleCategory/showCategory.jsx
+++ src/components/singleCategory/showCategory.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Table } from 'antd';
+import { Divider, Table } from 'antd';
 import { DeleteOutlined, UnorderedListOutlined } from '@ant-design/icons';
 
 import styles from './category.module.css';
@@ -8,17 +8,14 @@ import 'antd/dist/antd.css';
 import { showSource, showColumns } from './dummyData';
 
 const ShowCategory = ({ showEventList }) => {
-  // TODO: style display of most recent category occurance
 
   return (
     <div className={styles.showContainer}>
-      <Table 
-        dataSource={showSource}
-        columns={showColumns}
-        pagination={false}
-      />
-      <div>Most Recent Event</div>
-      September 19th, 2019
+      <Table dataSource={showSource} columns={showColumns} pagination={false} />
+      <div className={styles.mostRecent}>
+        <Divider>Most Recent Event</Divider>
+        September 19th, 2019
+      </div>
       <div className={styles.showIconsContainer}>
         <div className={styles.showIcon}>
           <DeleteOutlined />
