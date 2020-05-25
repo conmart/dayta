8b0f51206f6bb988c0fc37be0f1e87019088cace
conmart <connor.a.martinelli@gmail.com>
2020-05-25 14:24:56 -0700

    auth provider properly imported

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import React from 'react';
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+import { FirebaseAuthProvider } from '@react-firebase/auth';
 
-import { StateProvider } from "./state";
-import { initialState } from "./state/initialState";
-import { reducer } from "./state/reducer";
-
+import { StateProvider } from './state';
+import { initialState } from './state/initialState';
+import { reducer } from './state/reducer';
 import AppRouter from './AppRouter';
+import { firebaseConfig } from './firebaseConfig';
 
 import styles from './app.module.css';
 
 const App = () => (
-  <StateProvider initialState={initialState} reducer={reducer}>
-    <div className={styles.mobileAppContainer}>
-      <AppRouter />
-    </div>
-  </StateProvider>
+  <FirebaseAuthProvider firebase={firebase} {...firebaseConfig}>
+    <StateProvider initialState={initialState} reducer={reducer}>
+      <div className={styles.mobileAppContainer}>
+        <AppRouter />
+      </div>
+    </StateProvider>
+  </FirebaseAuthProvider>
 );
 
 export default App;
