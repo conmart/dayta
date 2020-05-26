12c707bed3ea2b83afcba8ce3a0a60e7bd18c539
conmart <connor.a.martinelli@gmail.com>
2020-05-25 19:16:09 -0700

    add firestore to app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import firebase from 'firebase/app';
 import 'firebase/auth';
+import 'firebase/firestore';
 import { FirebaseAuthProvider } from '@react-firebase/auth';
+import { FirestoreProvider } from '@react-firebase/firestore';
 
 import { StateProvider } from './state';
 import { initialState } from './state/initialState';
@@ -13,11 +15,13 @@ import styles from './app.module.css';
 
 const App = () => (
   <FirebaseAuthProvider firebase={firebase} {...firebaseConfig}>
-    <StateProvider initialState={initialState} reducer={reducer}>
-      <div className={styles.mobileAppContainer}>
-        <AppRouter />
-      </div>
-    </StateProvider>
+    <FirestoreProvider firebase={firebase} {...firebaseConfig}>
+      <StateProvider initialState={initialState} reducer={reducer}>
+        <div className={styles.mobileAppContainer}>
+          <AppRouter />
+        </div>
+      </StateProvider>
+    </FirestoreProvider>
   </FirebaseAuthProvider>
 );
 
